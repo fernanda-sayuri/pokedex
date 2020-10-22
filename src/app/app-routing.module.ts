5045023b8588cc4a6638030f05d7810998bd893e
fernanda-sayuri <fernanda.sayuri.ito@accenture.com>
2020-10-22 11:07:06 -0300

    create component card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './views/details/details.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component:  HomeComponent},
+  { path: "detalhes", component: DetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './views/home/home.component';
+import { DetailsComponent } from './views/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
